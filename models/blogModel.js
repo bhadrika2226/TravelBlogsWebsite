1647fcbdf681821a00fcbe08f645a558cc71196e
Nandini <nandiniparmar57@gmail.com>
2023-10-18 15:22:18 +0530

    blog_post

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,90 +1,104 @@
-const Blog = require('../models/blog'); // Import the Blog model
+// const Blog = require('../models/blog'); // Import the Blog model
 
-// Create a new blog post
-exports.createBlog = (req, res) => {
-  const { userId, title, state, city, date, bImg, description, tags } = req.body;
+// // Create a new blog post
+// exports.createBlog = (req, res) => {
+//   const { userId, title, state, city, date, bImg, description, tags } = req.body;
 
-  const newBlog = new Blog({
-    userId,
-    title,
-    state,
-    city,
-    date,
-    bImg,
-    description,
-    tags,
-  });
+//   const newBlog = new Blog({
+//     userId,
+//     title,
+//     state,
+//     city,
+//     date,
+//     bImg,
+//     description,
+//     tags,
+//   });
 
-  newBlog
-    .save()
-    .then((blog) => {
-      res.status(201).json(blog);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Internal server error' });
-    });
-};
+//   newBlog
+//     .save()
+//     .then((blog) => {
+//       res.status(201).json(blog);
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: 'Internal server error' });
+//     });
+// };
 
-// Get all blog posts
-exports.getAllBlogs = (req, res) => {
-  Blog.find()
-    .populate('userId', 'username') // Populate the 'userId' field with the 'username'
-    .exec()
-    .then((blogs) => {
-      res.status(200).json(blogs);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Internal server error' });
-    });
-};
+// // Get all blog posts
+// exports.getAllBlogs = (req, res) => {
+//   Blog.find()
+//     .populate('userId', 'username') // Populate the 'userId' field with the 'username'
+//     .exec()
+//     .then((blogs) => {
+//       res.status(200).json(blogs);
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: 'Internal server error' });
+//     });
+// };
 
-// Get a single blog post by ID
-exports.getBlogById = (req, res) => {
-  const blogId = req.params.id;
+// // Get a single blog post by ID
+// exports.getBlogById = (req, res) => {
+//   const blogId = req.params.id;
 
-  Blog.findById(blogId)
-    .populate('userId', 'username') // Populate the 'userId' field with the 'username'
-    .exec()
-    .then((blog) => {
-      if (!blog) {
-        return res.status(404).json({ message: 'Blog not found' });
-      }
-      res.status(200).json(blog);
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Internal server error' });
-    });
-};
+//   Blog.findById(blogId)
+//     .populate('userId', 'username') // Populate the 'userId' field with the 'username'
+//     .exec()
+//     .then((blog) => {
+//       if (!blog) {
+//         return res.status(404).json({ message: 'Blog not found' });
+//       }
+//       res.status(200).json(blog);
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: 'Internal server error' });
+//     });
+// };
 
-// Update a blog post by ID
-exports.updateBlog = (req, res) => {
-  const blogId = req.params.id;
-  const updateOps = {};
+// // Update a blog post by ID
+// exports.updateBlog = (req, res) => {
+//   const blogId = req.params.id;
+//   const updateOps = {};
 
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
+//   for (const ops of req.body) {
+//     updateOps[ops.propName] = ops.value;
+//   }
 
-  Blog.findByIdAndUpdate(blogId, { $set: updateOps })
-    .exec()
-    .then((result) => {
-      res.status(200).json({ message: 'Blog updated' });
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Internal server error' });
-    });
-};
+//   Blog.findByIdAndUpdate(blogId, { $set: updateOps })
+//     .exec()
+//     .then((result) => {
+//       res.status(200).json({ message: 'Blog updated' });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: 'Internal server error' });
+//     });
+// };
 
-// Delete a blog post by ID
-exports.deleteBlog = (req, res) => {
-  const blogId = req.params.id;
+// // Delete a blog post by ID
+// exports.deleteBlog = (req, res) => {
+//   const blogId = req.params.id;
 
-  Blog.findByIdAndRemove(blogId)
-    .exec()
-    .then((result) => {
-      res.status(200).json({ message: 'Blog deleted' });
-    })
-    .catch((error) => {
-      res.status(500).json({ error: 'Internal server error' });
-    });
-};
+//   Blog.findByIdAndRemove(blogId)
+//     .exec()
+//     .then((result) => {
+//       res.status(200).json({ message: 'Blog deleted' });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: 'Internal server error' });
+//     });
+// };
+
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  state: String,
+  city: String,
+  description: String,
+  image : String,
+  d: Date,
+  
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
